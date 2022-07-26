0a47be93946c25b8e1822d870b3e1be9b09d35cc
jotaerre01 <jruiz967@gmail.com>
2022-07-26 14:09:38 -0500

    security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,35 @@
+// origin-when-cross-origin
+// style-src 'self' example.com;
+// child-src example.com;
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  font-src 'self';  
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+  { key: 'X-XSS-Protection', value: '1; mode=block' },
+  { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+  { key: 'Feature-Policy', value: 'geolocation ' + '\'none\'' },
+  { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+  { key: 'X-DNS-Prefetch-Control', value: 'on' }
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // security
+  async headers() {
+    return [{
+      source: '/:path*',
+      headers: securityHeaders,
+    }]
+  },
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com'],
